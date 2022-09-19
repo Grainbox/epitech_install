b27ecbb9aa325addd194a62fe8496c75d223914f
Tom <tom.daniel@epitech.eu>
2022-09-19 16:43:02 +0200

    feat done

--- packages_install.sh
+++ packages_install.sh
@@ -98,3 +98,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu `lsb_release -cs` test"
 apt update
 apt install docker-ce
+
+echo DONE
